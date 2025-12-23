68cebec25e90cad93499ed34eebb03868c52230f
Eyad <50059804+Eabusham2@users.noreply.github.com>
2025-12-22 19:05:43 -0600

    Update BannerBlockEntityRendererMixin.java
    fixed/updated for 1.21.5

--- src/main/java/dev/stardust/mixin/BannerBlockEntityRendererMixin.java
+++ src/main/java/dev/stardust/mixin/BannerBlockEntityRendererMixin.java
@@ -1,39 +1,38 @@
 package dev.stardust.mixin;
 
-import net.minecraft.text.Text;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import net.minecraft.client.util.math.MatrixStack;
-import org.spongepowered.asm.mixin.injection.Inject;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import meteordevelopment.meteorclient.systems.modules.render.NoRender;
 import net.minecraft.block.entity.BannerBlockEntity;
 import net.minecraft.client.render.VertexConsumerProvider;
-import meteordevelopment.meteorclient.systems.modules.Modules;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.Vec3d;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import meteordevelopment.meteorclient.systems.modules.render.NoRender;
 import net.minecraft.client.render.block.entity.BannerBlockEntityRenderer;
 
-/**
- * @author Tas [0xTas] <root@0xTas.dev>
- **/
 @Mixin(BannerBlockEntityRenderer.class)
 public class BannerBlockEntityRendererMixin {
+    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
+    private void onRender(BannerBlockEntity bannerBlockEntity, float tickDelta, MatrixStack matrices,
+                          VertexConsumerProvider vertexConsumers, Vec3d pos, int light, int overlay,
+                          CallbackInfo ci) {
+        if (bannerBlockEntity.getWorld() == null) return;
 
-    // See NoRenderMixin.java
-    @Inject(method = "render(Lnet/minecraft/block/entity/BannerBlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;II)V", at = @At("HEAD"), cancellable = true)
-    private void onRender(BannerBlockEntity bannerBlockEntity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay, CallbackInfo ci) {
-        if (bannerBlockEntity.getWorld() != null) {
-            Modules mods = Modules.get();
-            if (mods == null) return;
-            NoRender noRender = mods.get(NoRender.class);
-            if (!noRender.isActive()) return;
+        Modules mods = Modules.get();
+        if (mods == null) return;
 
-            Text bannerName = bannerBlockEntity.getCustomName();
-            var bannerSetting = noRender.settings.get("cody-banners");
+        NoRender noRender = mods.get(NoRender.class);
+        if (noRender == null || !noRender.isActive()) return;
 
-            if (bannerSetting == null || bannerName == null) return;
-            if ((boolean) bannerSetting.get() && bannerName.getString().contains("codysmile11")) {
-                ci.cancel();
-            }
+        Text bannerName = bannerBlockEntity.getCustomName();
+        var bannerSetting = noRender.settings.get("cody-banners");
+        if (bannerSetting == null || bannerName == null) return;
+
+        if ((boolean) bannerSetting.get() && bannerName.getString().contains("codysmile11")) {
+            ci.cancel();
         }
     }
 }
