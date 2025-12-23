6b57de275588fddfb9f60ed9b0e5f2dc898d35c7
Eyad <50059804+Eabusham2@users.noreply.github.com>
2025-12-22 19:04:59 -0600

    Update WorldMixin.java
    1.21.5

--- src/main/java/dev/stardust/mixin/WorldMixin.java
+++ src/main/java/dev/stardust/mixin/WorldMixin.java
@@ -1,35 +1,35 @@
 package dev.stardust.mixin;
 
-import net.minecraft.world.World;
 import dev.stardust.modules.AutoSmith;
+import dev.stardust.modules.StashBrander;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.world.WorldAccess;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
 import org.spongepowered.asm.mixin.Mixin;
-import net.minecraft.sound.SoundCategory;
-import dev.stardust.modules.StashBrander;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-/**
- * @author Tas [0xTas] <root@0xTas.dev>
- **/
 @Mixin(World.class)
 public abstract class WorldMixin implements WorldAccess, AutoCloseable {
-    // See StashBrander.java && AutoSmith.java
-    @Inject(method = "playSoundAtBlockCenter", at = @At("HEAD"), cancellable = true)
-    private void mixinPlaySoundAtBlockCenter(BlockPos pos, SoundEvent sound, SoundCategory category, float volume, float pitch, boolean useDistance, CallbackInfo ci) {
+    @Inject(method = "playSoundAtBlockCenterClient", at = @At("HEAD"), cancellable = true)
+    private void mixinPlaySoundAtBlockCenterClient(BlockPos pos, SoundEvent sound, SoundCategory category,
+                                                   float volume, float pitch, boolean useDistance,
+                                                   CallbackInfo ci) {
         Modules modules = Modules.get();
         if (modules == null) return;
+
         AutoSmith smith = modules.get(AutoSmith.class);
         StashBrander brander = modules.get(StashBrander.class);
-        if (brander.isActive() && brander.shouldMute()) {
+
+        if (brander != null && brander.isActive() && brander.shouldMute()) {
             if (sound == SoundEvents.BLOCK_ANVIL_USE || sound == SoundEvents.BLOCK_ANVIL_BREAK) ci.cancel();
         }
-        if (smith.isActive() && smith.muteSmithy.get()) {
+        if (smith != null && smith.isActive() && smith.muteSmithy.get()) {
             if (sound == SoundEvents.BLOCK_SMITHING_TABLE_USE) ci.cancel();
         }
     }
